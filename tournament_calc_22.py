19f19e1889ddc80c7e261caa405d725d358c1875
Varsus808 <micha.borodin@gmail.com>
2022-11-07 20:45:49 +0100

    finalized

--- tournament_calc_22.py
+++ tournament_calc_22.py
@@ -1,6 +1,8 @@
-
+import functools
+import typing
 import requests
 import urllib.parse
+import time
 import sys
 # bot.py
 import os
@@ -82,36 +84,43 @@ def simplify_line(string):
 
 
 def deckcheck(decklist):
-	base_url = "https://api.scryfall.com/cards/search?q=" + urllib.parse.quote("(r:u or r:c) (set:sunf OR set:unf OR set:und OR set:ust OR set:unh OR set:ugl)")
-	out_errors = ""
+	base_url = "https://api.scryfall.com/cards/search?q=" + urllib.parse.quote("(r:u or r:c) -(set:sunf OR set:unf OR set:und OR set:ust OR set:unh OR set:ugl)")
+	out = "```diff\n"
+
 	
-	print(type(decklist))
 	for line in decklist:
-		print(decklist, line)
+		
 		simple = simplify_line(line)
-		print(simple)
 		if simple in banned_list:
-			out_errors += f"{simple} is either too similar to an entry in the Banned List or actually illegal \n"
+			out += f"-{simple}\n"
 		else:
 			api_url = base_url + urllib.parse.quote(simple)
 			response = requests.get(api_url)
+			#time.sleep(5)
 			data =response.json()
 			if data["object"] == "error" or data["total_cards"] == 0:
-				out_errors += f"{simple} Seems not to be common or uncommon, or you are using un-Set cards!\n"
+				print(data)
+				print(api_url)
+				out += f"-{simple}\n"
+			else: 
+				out += f"+{simple}\n"
+	out += "```\n"
+	return out
 
-	return out_errors
+
+async def run_blocking(blocking_func: typing.Callable, *args, **kwargs) -> typing.Any:
+    """Runs a blocking function in a non-blocking way"""
+    func = functools.partial(deckcheck, *args, **kwargs) # `run_in_executor` doesn't support kwargs, `functools.partial` does
+    return await client.loop.run_in_executor(None, func)
 
 @client.event
 async def on_message(message):
 	if message.author == client.user:
 		return
-	usr_message = message.content.splitlines()
-	usr_message_1 = usr_message[:len(usr_message)//2]
-	usr_message_2 = usr_message[len(usr_message)//2:]
-	response = deckcheck(usr_message_1)
-	await message.channel.send(response)
-	response = deckcheck(usr_message_2)
-	await message.channel.send(response)
+	await message.channel.send("Please give me some Minutes<3```diff\n+Green cards are legal\n-Red cards are illegal```\n")
+
+	text = await run_blocking(deckcheck, usr_message[:])
+	await message.channel.send(text)
 
 
 client.run(TOKEN)
